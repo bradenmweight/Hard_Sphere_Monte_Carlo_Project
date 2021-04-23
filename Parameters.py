a25ee416862758fdf9516e9d3853b3f0131ddcb9
bradenmweight <69997194+bradenmweight@users.noreply.github.com>
2021-04-23 03:14:05 -0400

    Implemented Fixed boundaries. However, something is wrong with them at the moment. Still the gas expands without bound.

--- Parameters.py
+++ Parameters.py
@@ -4,9 +4,15 @@ class Parameters():
 
     ### SIMULATION DETAILS ###
     dimensions = 3
-    NParticles = 22
-    particleDiameter = 1.0
-    latticeType="SC"
-    latticeLength=20
-    NSteps = 10
-    stepSize = 0.1
+    NParticles = 20 # Code will round up to nearest square or cube
+    particleDiameter = 2.0
+    latticeType="SC" # Only "SC" is implemented
+    latticeLength=10
+    NSteps = 10000
+    stepSize = 0.2
+    kb = 1
+    Temp = 2 # Reduced Units
+
+    boundaryType = 'Fixed' # "PBC" not yet implemented
+
+
