e9886644b350f8cb41ab15b83d49a777bd3b0832
bradenmweight <69997194+bradenmweight@users.noreply.github.com>
2021-04-22 22:53:11 -0400

    Parameters model will house simulation parameters.

---
+++ Parameters.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+class Parameters():
+
+    ### SIMULATION DETAILS ###
+    dimensions = 3
+    NParticles = 22
+    particleDiameter = 1.0
+    latticeType="SC"
+    latticeLength=20
+    NSteps = 10
+    stepSize = 0.1
