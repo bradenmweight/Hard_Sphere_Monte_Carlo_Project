9d5d1afc859d38f47acb59ef436be7801e16a7fb
bradenmweight <69997194+bradenmweight@users.noreply.github.com>
2021-04-22 23:00:11 -0400

    Fixed out to conform to standard XYZ format.

--- Writer.py
+++ Writer.py
@@ -1,8 +1,14 @@
 import numpy as np
 
 def writeCoords(step, coords, geomFile):
+    geomFile.write( str(len(coords)) + "\n")
+    geomFile.write( "Step: " + str(step) + "\n" )
     for n in range(len(coords)):
         outArray = [step, coords[n,0], coords[n,1], coords[n,2] ]
         geomFile.write( "\t".join(map(str,outArray)) + "\n" )
+    geomFile.write( "\n" )
+
+
+
 
 
