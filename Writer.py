bff2b23bb37b7ab923e6a937334a2d00045548d3
bradenmweight <69997194+bradenmweight@users.noreply.github.com>
2021-04-22 23:08:34 -0400

    Fixed bug. Added particle type "1" for all particles.

--- Writer.py
+++ Writer.py
@@ -4,9 +4,12 @@ def writeCoords(step, coords, geomFile):
     geomFile.write( str(len(coords)) + "\n")
     geomFile.write( "Step: " + str(step) + "\n" )
     for n in range(len(coords)):
-        outArray = [step, coords[n,0], coords[n,1], coords[n,2] ]
-        geomFile.write( "\t".join(map(str,outArray)) + "\n" )
-    geomFile.write( "\n" )
+        outArray = np.array( [1.0, coords[n,0], coords[n,1], coords[n,2] ] )
+        geomFile.write( "\t".join(map(str,np.round(outArray,5))) + "\n" )
+    #geomFile.write( "\n" )
+
+
+
 
 
 
