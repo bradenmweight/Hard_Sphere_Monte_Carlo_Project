ea85e1269ca9af8d771f5c48b105f822de944b4f
bradenmweight <bweight@ur.rochester.edu>
2021-04-27 03:02:41 -0400

    Added PBC and Lennard-Jones potential

--- Parameters.py
+++ Parameters.py
@@ -3,16 +3,21 @@ import numpy as np
 class Parameters():
 
     ### SIMULATION DETAILS ###
-    dimensions = 3
-    NParticles = 20 # Code will round up to nearest square or cube
-    particleDiameter = 2.0
+    dimensions = 2
+    NParticles = 100 # Code will round up to nearest square (2D) or cube (3D)
+    particleDiameter = 1.0
     latticeType="SC" # Only "SC" is implemented
-    latticeLength=10
-    NSteps = 10000
+    latticeLength=20
+    NSteps = 5000
     stepSize = 0.2
-    kb = 1
-    Temp = 2 # Reduced Units
+    kb, Temp = 1, 2 # Reduced Units
 
-    boundaryType = 'Fixed' # "PBC" not yet implemented
+    boundaryType = 'PBC' # "Fixed", "PBC"
+    potentialType = "LJ" # "Hard" , "LJ", "Free"
+
+    # For LJ potential, define well parameter
+    # Sigma taken to be diameter of particles
+    eps = 5.0
+
 
 
