693416f3a1dfddadee428c21d1c9b3203c4fcf47
alikhan-yeltokov <83046566+alikhan-yeltokov@users.noreply.github.com>
2021-04-30 00:35:18 -0400

    Update initialize.py

--- initialize.py
+++ initialize.py
@@ -1,8 +1,8 @@
 import numpy as np
-
+import matplotlib.pyplot as plt
 import Parameters
 
-def initParticles( NParticles=Parameters.Parameters.NParticles, latticeType="SC", 
+def initParticles( NParticles=Parameters.Parameters.NParticles, latticeType=Parameters.Parameters.latticeType, 
         dimensions=Parameters.Parameters.dimensions, latticeLength=Parameters.Parameters.latticeLength ):
     """
     Initialize particles according to specified lattice arrangement and size.
@@ -17,36 +17,78 @@ def initParticles( NParticles=Parameters.Parameters.NParticles, latticeType="SC"
     coords:         nd.array() -- shape = NParticles x 4 = Type, x,y,z
 
     """
-    
-    # Simple Cubic Lattice
-    assert ( latticeType == "SC" and dimensions != 1 ), "Only 2D and 3D SC lattice is currently implemented."
-    
     L = latticeLength
     radius = Parameters.Parameters.particleDiameter / 2
 
     coords = []
-    if ( dimensions == 2 ):
-        # Get important properties
-        L12 = L / NParticles ** (1/2)
-        n = NParticles / L ** 2 # Particle density
-        phi = NParticles * np.pi * radius**2 / L**2 # Volume of particles / Volume of box
-
-        for i in np.arange( 0, L, L12 ):
-            for j in np.arange( 0, L, L12 ):
-                coords.append([ 1,i,j ])
-
-    elif ( dimensions == 3 ):
-        # Get important properties
+    if(latticeType=="SC"):
+        if ( dimensions == 2 ):
+            # Get important properties
+            L12 = L / NParticles ** (1/2)
+            for i in np.arange( 0, L, L12 ):
+                for j in np.arange( 0, L, L12 ):
+                    coords.append([ 1,i,j ])
+            n = len(coords) / L ** 2 # Particle density
+            phi = len(coords) * np.pi * radius**2 / L**2 # Volume of particles / Volume of box
+        
+        elif ( dimensions == 3 ):
+         # Get important properties
+            L13 = L / NParticles ** (1/3)
+            for i in np.arange( 0, L, L13 ):
+                for j in np.arange( 0, L, L13 ):
+                    for k in np.arange( 0, L, L13 ):
+                        coords.append([ 1,i,j,k ])
+            for i in np.arange( 0, L-L13, L13 ):
+                for j in np.arange( 0, L-L13, L13 ):
+                    for k in np.arange( 0, L-L13, L13 ):
+                        coords.append([ 1,i+L13/2,j+L13/2,k+L13/2 ])
+            n = len(coords) / L ** 3 # Particle density
+            phi = len(coords) * (4/3) * np.pi * radius**3 / L**3
+        coords = np.array(coords)
+    elif(latticeType=="BCC"):
         L13 = L / NParticles ** (1/3)
-        n = NParticles / L ** 3 # Particle density
-        phi = NParticles * (4/3) * np.pi * radius**3 / L**3
-
-        for i in np.arange( 0, L, L13 ):
+        for i in np.arange( 0, L, L13 ): #edge atoms
             for j in np.arange( 0, L, L13 ):
                 for k in np.arange( 0, L, L13 ):
                     coords.append([ 1,i,j,k ])
-
-    coords = np.array(coords)
+        for i in np.arange( 0, L-L13, L13 ): #center atoms
+            for j in np.arange( 0, L-L13, L13 ):
+                for k in np.arange( 0, L-L13, L13 ):
+                    coords.append([ 1,i+L13/2,j+L13/2,k+L13/2 ])
+        coords = np.array(coords)
+        n = len(coords) / L ** 3 # Particle density
+        phi = len(coords) * (4/3) * np.pi * radius**3 / L**3
+    elif(latticeType=="FCC"):
+        # Get important properties
+        L13 = L / NParticles ** (1/3)
+        for i in np.arange( 0, L, L13 ): #lattice edges atoms
+            for j in np.arange( 0, L, L13 ):
+                for k in np.arange( 0, L, L13 ):
+                    coords.append([ 1,i,j,k ])
+        for i in np.arange( 0, L-L13/2, L13 ): #top bottom atoms
+            for j in np.arange( 0, L-L13/2, L13 ):
+                for k in np.arange( 0, L, L13 ):
+                    #coords.append([ 1,i*L13+L13/2,j*L13+L13/2,k*L13+L13/2 ])
+                    coords.append([ 1,i+L13/2,j+L13/2,k ])
+        for i in np.arange( 0, L, L13 ): #front rear atoms
+            for j in np.arange( 0, L-L13/2, L13 ):
+                for k in np.arange( 0, L-L13/2, L13 ):
+                    #coords.append([ 1,i*L13+L13/2,j*L13+L13/2,k*L13+L13/2 ])
+                    coords.append([ 1,i,j+L13/2,k +L13/2])
+        for i in np.arange( 0, L-L13/2, L13 ): #left right atoms
+            for j in np.arange( 0, L, L13 ):
+                for k in np.arange( 0, L-L13/2, L13 ):
+                    #coords.append([ 1,i*L13+L13/2,j*L13+L13/2,k*L13+L13/2 ])
+                    coords.append([ 1,i+L13/2,j,k +L13/2])
+        for i in np.arange( 0, L-L13, L13/2 ): #center atoms
+            for j in np.arange( 0, L-L13, L13/2 ):
+                for k in np.arange( 0, L-L13, L13/2 ):
+                    #coords.append([ 1,i*L13+L13/2,j*L13+L13/2,k*L13+L13/2 ])
+                    coords.append([ 1,i+L13/2,j+L13/2,k+L13/2 ])
+        n = len(coords) / L ** 3 # Particle density
+        phi = len(coords) * (4/3) * np.pi * radius**3 / L**3
+        coords = np.array(coords)
+     
 
     #print ("The particle density is %5.4f" % (phi) )
     np.savetxt("geometry0.xyz", coords)
@@ -66,3 +108,4 @@ def initParticles( NParticles=Parameters.Parameters.NParticles, latticeType="SC"
 
 if ( __name__ == "__main__" ):
     initParticles()
+  
