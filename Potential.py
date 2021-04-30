Fixed formula for dR

Potential.py
@@ -42,7 +42,7 @@ def getV( coords, Lmin, Lmax ):
         for n in range( len(coords) ):
             for m in range( n+1, len(coords) ):
                 if ( boundaryType == "PBC" ):
-                    dR = np.array([ coords[n,d+1] % (Lmax[d]-Lmin[d]) - coords[m,d+1] % (Lmax[d]-Lmin[d]) for d in range(dimensions) ])
+                    dR = np.array([ (Lmax[d]-Lmin[d])%(coords[n,d+1] - coords[m,d+1]) for d in range(dimensions) ])
                 else:
                     dR = np.array([ coords[n,d+1] - coords[m,d+1] for d in range(dimensions) ])
                 r = np.linalg.norm( dR )
@@ -57,7 +57,7 @@ def getV( coords, Lmin, Lmax ):
         for n in range( len(coords) ):
             for m in range( n+1, len(coords) ):
                 if ( boundaryType == "PBC" ):
-                    dR = np.array([ coords[n,d+1] % (Lmax[d]-Lmin[d]) - coords[m,d+1] % (Lmax[d]-Lmin[d]) for d in range(dimensions) ])
+                    dR = np.array([ (Lmax[d]-Lmin[d])%(coords[n,d+1] - coords[m,d+1]) for d in range(dimensions) ])
                 r = np.linalg.norm( dR )
                 V += 4*eps*( (Diam/r)**12 - (Diam/r)**6 )
         return V
@@ -84,7 +84,7 @@ def getVSingleParticle( coords, Lmin, Lmax, ind):
         for n in range( len(coords) ):
             if ( n != ind ):
                 if ( boundaryType == "PBC" ):
-                    dR = np.array([ coords[n,d+1] % (Lmax[d]-Lmin[d]) - coords[ind,d+1] % (Lmax[d]-Lmin[d]) for d in range(dimensions) ])
+                    dR = np.array([ (Lmax[d]-Lmin[d])%(coords[n,d+1] - coords[m,d+1]) for d in range(dimensions) ])
                 else:
                     dR = np.array([ coords[n,d+1] - coords[ind,d+1] for d in range(dimensions) ])
                 r = np.linalg.norm( dR )
@@ -99,7 +99,7 @@ def getVSingleParticle( coords, Lmin, Lmax, ind):
         for n in range( len(coords) ):
             if ( n != ind ):
                 if ( boundaryType == "PBC" ):
-                    dR = np.array([ coords[n,d+1] % (Lmax[d]-Lmin[d]) - coords[ind,d+1] % (Lmax[d]-Lmin[d]) for d in range(dimensions) ])
+                    dR = np.array([ (Lmax[d]-Lmin[d])%(coords[n,d+1] - coords[m,d+1]) for d in range(dimensions) ])
                 r = np.linalg.norm( dR )
                 V += 4*eps*( (Diam/r)**12 - (Diam/r)**6 )
         return V