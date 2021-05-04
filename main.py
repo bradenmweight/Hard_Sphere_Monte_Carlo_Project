ed2c9a3c8fe3a81d10a9f121dc2e70e70d0916c6
bradenmweight <bweight@ur.rochester.edu>
2021-05-04 13:40:07 -0400

    Changed default MC algorithm to more efficient single-particle propagation and implemented initializing particles from volume fraction condition.

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 # This will be the main file for this package.
-# Chloe can edit
 
 import numpy as np
 
@@ -20,22 +19,22 @@ import Parameters
     # If accepted, compute necessary information: pressure, temperature, etc. g(r), SSF, etc.
 
 # ANALYSIS
-#   1. Radial Distrution Function   --> radial_dist.dat
-#   2. Static Structure Factor      --> 
-#   3. Compute time-correlation    --> 
-#   4. SSF --> Phase Diagram via Hansen and Verlet freezing criterion (height of first peak in SSF)
+#   1. Radial Distrution Function   --> radial_dist.py
+#   2. Static Structure Factor      --> SSF.py
+#   3. Compute time-correlation     --> getAutoCorrelation.py
+#   (SKIP) 4. SSF --> Phase Diagram via Hansen and Verlet freezing criterion (height of first peak in SSF)
 
 # POSSIBLE FUTURE IMPROVEMENTS
-#   1. Implement periodic boundary conditions: boundaryType = "Fixed", "PBC"
+#   1. Implement periodic boundary conditions: boundaryType = "Fixed", "PBC" -- DONE -- BMW
 #   2. Implement different initial lattices: lattiveType = "SC", "FCC", "BCC", "Diamond"
-#   3. Additional potential types: Lennard-Jones (6-12)
-#   4. Implement 1-Dimensional code
+#   3. Additional potential types: Lennard-Jones (6-12) -- DONE -- BMW
+#   (SKIP )4. Implement 1-Dimensional code
 
 if ( __name__ == "__main__" ):
 
     # Initialize the particles
-    coords, Lmin, Lmax  = initialize.initParticles()
-    #MC.runMC( coords, Lmin, Lmax ) # This runs as follows: (I) Move all particles randomly (II) Compute probability
+    coords, Lmin, Lmax  = initialize.initParticles_fromPHI0()
+    #MC.runMC( coords, Lmin, Lmax ) # This runs as follows: (I) Move all particles randomly (II) Compute probability -- DO NOT USE THIS ONE.
     MC.runMCSingleParticleVersion( coords, Lmin, Lmax )  # This runs as follows: (I) Move a single particle randomly (II) Compute probability
 
 
