527a832d98e153fb5cd9b7fb5f35162cb62a236c
bradenmweight <bweight@ur.rochester.edu>
2021-05-04 13:44:00 -0400

    Slightly updated. Major: latticeLength" is now deprecated.

--- Parameters.py
+++ Parameters.py
@@ -2,22 +2,23 @@ import numpy as np
 
 class Parameters():
 
-    ### SIMULATION DETAILS ###
-    dimensions = 2
-    NParticles = 100 # Code will round up to nearest square (2D) or cube (3D)
+    dimensions = 3
+    NParticles = 100
     particleDiameter = 1.0
-    latticeType="SC" # Only "SC" is implemented
-    latticeLength=20
-    NSteps = 5000
+    latticeType="SC" # Only "SC", "FCC" is implemented
+    #latticeLength=9
+    phi0 = 0.20
+    NSteps = int(10**6)
     stepSize = 0.2
-    kb, Temp = 1, 2 # Reduced Units
+    kb, Temp = 1, 1 # Reduced Units
 
     boundaryType = 'PBC' # "Fixed", "PBC"
     potentialType = "LJ" # "Hard" , "LJ", "Free"
 
     # For LJ potential, define well parameter
     # Sigma taken to be diameter of particles
-    eps = 5.0
+    eps = 0.5
+
 
 
 
